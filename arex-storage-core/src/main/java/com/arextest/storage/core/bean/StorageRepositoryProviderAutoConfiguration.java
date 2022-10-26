1b51233ee1f2318f7545aeb1200c0dd8be844f27
mok <hanjian2086@gmail.com>
2022-10-27 01:15:33 +0800

    move beans to core package

---
+++ arex-storage-core/src/main/java/com/arextest/storage/core/bean/StorageRepositoryProviderAutoConfiguration.java
@@ -0,0 +1,27 @@
+package com.arextest.storage.core.bean;
+
+
+import com.arextest.storage.core.repository.RepositoryProvider;
+import com.arextest.storage.core.repository.impl.mongo.MongoDbUtils;
+import com.mongodb.client.MongoDatabase;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnMissingBean(RepositoryProvider.class)
+@EnableConfigurationProperties({StorageMongoProperties.class})
+public class StorageRepositoryProviderAutoConfiguration {
+    private final StorageMongoProperties properties;
+
+    public StorageRepositoryProviderAutoConfiguration(StorageMongoProperties storageMongoProperties) {
+        properties = storageMongoProperties;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(MongoDatabase.class)
+    public MongoDatabase mongoRepository() {
+        return MongoDbUtils.create(properties.getHost(), properties.getDbName());
+    }
+}
